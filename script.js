adb809268a0a0f42fd3bf7f449c6480a30875090
OvernightSolo <sterlingtx75@gmail.com>
2022-09-24 13:49:13 -0500

    added save button functionality

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
 // Display the current day in the header
-$("#currentDay").text(moment().format("dddd, MMMM Do"));
+let todayEl = $("#currentDay");
+todayEl.text("Today is " + moment().format("dddd, MMMM Do"));
+
+// Creating click functionality to the save buttons
+$(document).ready(() => {
+  $("saveBtn").on("click", () => {
+    let value = $(this).siblings(".description").val();
+    let time = $(this).parent().attr("id");
+
+    //Save to local storage
+    localStorage.setItem(time, value);
+
+    // Display a notification that the item was saved to local storage
+    $(".notification").addClass("show");
+
+    // Turn notification off after three seconds
+    setTimeout(() => {
+      $(".notification").addClass("show");
+    }, 3000);
+  });
+});
