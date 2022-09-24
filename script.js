4464104b219da619ee4c9e392194860cdea1497b
OvernightSolo <sterlingtx75@gmail.com>
2022-09-24 17:00:57 -0500

    fixed the notification

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
-// Display the current day in the header
-let todayEl = $("#currentDay");
-todayEl.text("Today is " + moment().format("dddd, MMMM Do"));
-
-// Create click functionality to the save buttons
-$(document).ready(() => {
-  $("saveBtn").on("click", () => {
+$(document).ready(function () {
+  // Create click functionality to the save buttons
+  $(".saveBtn").on("click", function () {
     let value = $(this).siblings(".description").val();
     let time = $(this).parent().attr("id");
 
@@ -15,46 +11,50 @@ $(document).ready(() => {
     $(".notification").addClass("show");
 
     // Turn notification off after three seconds
-    setTimeout(() => {
-      $(".notification").addClass("show");
+    setTimeout(function () {
+      $(".notification").removeClass("show");
     }, 3000);
   });
+
+  function updateTime() {
+    // Determine current amount of hours
+    let currentBlock = moment().hours();
+
+    // Iterate over the blocks of time
+    $(".time-block").each(function () {
+      let timeBlock = parseInt($(this).attr("id").split("-")[1]);
+
+      // Determine whether or not we are past the desired time block
+      if (timeBlock < currentBlock) {
+        $(this).addClass("past");
+      } else if (timeBlock === currentBlock) {
+        $(this).removeClass("past");
+        $(this).addClass("present");
+      } else {
+        $(this).removeClass("past");
+        $(this).removeClass("present");
+        $(this).addClass("future");
+      }
+    });
+  }
+
+  updateTime();
+
+  // Check to see if the current time should be updated
+  // let timeInterval = setInterval(updateTime, 15000);
+
+  // Get items from local storage, if there is any
+  $("#9a .description").val(localStorage.getItem("9a"));
+  $("#10a .description").val(localStorage.getItem("10a"));
+  $("#11a .description").val(localStorage.getItem("11a"));
+  $("#12p .description").val(localStorage.getItem("12p"));
+  $("#1p .description").val(localStorage.getItem("1p"));
+  $("#2p .description").val(localStorage.getItem("2p"));
+  $("#3p .description").val(localStorage.getItem("3p"));
+  $("#4p .description").val(localStorage.getItem("4p"));
+  $("#5p .description").val(localStorage.getItem("5p"));
+
+  // Display the current day in the header
+  let todayEl = $("#currentDay");
+  todayEl.text("Today is " + moment().format("dddd, MMMM Do"));
 });
-
-function updateTime() {
-  // Determine current amount of hours
-  let currentBlock = moment().hours();
-
-  // Iterate over the blocks of time
-  $(".time-block").each(function () {
-    let timeBlock = parseInt($(this).attr("id").split("-")[1]);
-
-    // Determine whether or not we are past the desired time block
-    if (timeBlock < currentBlock) {
-      $(this).addClass(".past");
-    } else if (timeBlock === currentBlock) {
-      $(this).removeClass("past");
-      $(this).addClass("present");
-    } else {
-      $(this).removeClass("past");
-      $(this).removeClass("present");
-      $(this).addClass("future");
-    }
-  });
-}
-
-updateTime();
-
-// Check to see if the current time should be updated via interval
-let timeInterval = setInterval(updateTime, 15000);
-
-// Get items from local storage, if there is any
-$("#9a .description").val(localStorage.getItem("9a"));
-$("#10a .description").val(localStorage.getItem("10a"));
-$("#11a .description").val(localStorage.getItem("11a"));
-$("#12p .description").val(localStorage.getItem("12p"));
-$("#1p.description").val(localStorage.getItem("1p"));
-$("#2p .description").val(localStorage.getItem("2p"));
-$("#3p .description").val(localStorage.getItem("3p"));
-$("#4p .description").val(localStorage.getItem("4p"));
-$("#5p .description").val(localStorage.getItem("5p"));
